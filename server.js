7e457016134dcdf6f56139254eb246ead92ce643
COREGPT <aryaaarush77@gmail.com>
2025-07-02 05:30:34 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -8,13 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('CORE backend is live!');
-});
-
-app.post('/', (req, res) => {
-  const userMessage = req.body.message || '';
-  const reply = `You said: ${userMessage}`;
-  res.json({ reply });
+  res.send('CORE Backend is running!');
 });
 
 app.listen(PORT, () => {
